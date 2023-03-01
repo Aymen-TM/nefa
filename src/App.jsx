9d4536abdcb3c6ad52a8a78928d7fff74cb0d9f0
Aymen-TM <t_mustapha_aymen@outlook.com>
2023-03-01 11:35:41 +0100

    init

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Hero from "./containers/Hero/Hero"
+import NavBar from "./containers/NavBar/NavBar"
+
+
+function App() {
+
+  return (
+    <div>
+      <NavBar />
+      <Hero />
+    </div>
+  )
+}
+
+export default App
